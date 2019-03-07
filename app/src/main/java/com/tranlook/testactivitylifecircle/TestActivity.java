38ebbad0a9ae726f8ec2e3fc320ecf7cd8bf4cc3
phamvietduc1994 <phamvietduc1994@gmail.com>
2019-03-07 21:41:25 +0700

    first commit

---
+++ app/src/main/java/com/tranlook/testactivitylifecircle/TestActivity.java
@@ -0,0 +1,41 @@
+package com.tranlook.testactivitylifecircle;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "On Create");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "On Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "On Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "On Destroy");
+
+    }
+}
